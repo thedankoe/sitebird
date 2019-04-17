dd99b45c9e2241542b08e34a08e8cca43c3768c1
Dan Koe <thedankoe@gmail.com>
2019-04-16 20:51:29 -0700

    serverless try again againnn

---
+++ src/components/functions/checkout.js
@@ -0,0 +1,55 @@
+/* eslint-disable */
+
+
+exports.handler = (event, context, callback) => {
+  const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+  console.log("creating charge...");
+
+  // Pull out the amount and id for the charge from the POST
+  console.log(event);
+  const requestData = JSON.parse(event.body);
+  console.log(requestData);
+  const amount = requestData.amount;
+  const description = requestData.description
+  const token = requestData.token.id;
+
+  // Headers to prevent CORS issues
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type"
+  };
+
+  return stripe.charges
+    .create({
+      // Create Stripe charge with token
+      amount,
+      token,
+      currency: "usd",
+      description
+    })
+    .then(charge => {
+      // Success response
+      console.log(charge);
+      const response = {
+        headers,
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `Charge processed!`,
+          charge
+        })
+      };
+      callback(null, response);
+    })
+    .catch(err => {
+      // Error response
+      console.log(err);
+      const response = {
+        headers,
+        statusCode: 500,
+        body: JSON.stringify({
+          error: err.message
+        })
+      };
+      callback(null, response);
+    });
+};
